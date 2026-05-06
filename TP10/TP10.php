54df63ba37b67943650456b95a79a150c032c361
ya.kajouj <ya.kajouj@gmail.com>
2026-05-06 22:46:49 +0200

    j'ai fini le tp

---
+++ TP10/TP10.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>TP10 - Formulaire en JS</title>
+</head>
+<body>
+    <h1>TP10 - Formulaire en JS</h1>
+    <h2>Cimentier Lafarge</h2>
+
+</body>
+</html>
